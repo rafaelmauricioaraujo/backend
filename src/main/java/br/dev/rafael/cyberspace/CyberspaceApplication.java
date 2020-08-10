cdfaa4420236e1eb1f94b0464c5c73431b1c39b5
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-08-10 20:56:31 -0300

    starting project

---
+++ src/main/java/br/dev/rafael/cyberspace/CyberspaceApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rafael.cyberspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rafael/cyberspace/CyberspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rafael.cyberspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
