a88c3c70f36f19ec5db7939dd4a68b40ddf83228
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-08-11 21:58:02 -0300

    created topicos dto

--- src/main/java/br/dev/rafael/cyberspace/controller/TopicosController.java
+++ src/main/java/br/dev/rafael/cyberspace/controller/TopicosController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.rafael.cyberspace.controller.dto.TopicoDto;
 import br.dev.rafael.cyberspace.model.Curso;
 import br.dev.rafael.cyberspace.model.Topico;
 
@@ -13,9 +14,9 @@ import br.dev.rafael.cyberspace.model.Topico;
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	public List<Topico> lista(){
+	public List<TopicoDto> lista(){
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 
 }
