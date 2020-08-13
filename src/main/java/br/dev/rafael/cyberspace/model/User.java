74459c0e5f0903228e73dcba20e6bd45a9033472
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-08-13 15:42:16 -0300

    entity mapping

--- src/main/java/br/dev/rafael/cyberspace/model/User.java
+++ src/main/java/br/dev/rafael/cyberspace/model/User.java
@@ -1,7 +1,14 @@
 package br.dev.rafael.cyberspace.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
