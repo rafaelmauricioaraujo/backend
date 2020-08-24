db1b27efc7ca3bf46bfc57316dc13e8629364b91
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-08-24 17:06:46 -0300

    security config

---
+++ src/main/java/br/dev/rafael/cyberspace/config/security/TokenService.java
@@ -0,0 +1,36 @@
+package br.dev.rafael.cyberspace.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.dev.rafael.cyberspace.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${cyberspace.jwt.expiration}")
+	private String expiration;
+
+	@Value("${cyberspace.jwt.secret}")
+	private String secret;
+	
+	public String generateToken(Authentication authentication) {
+		User user = (User) authentication.getPrincipal();
+		Date today = new Date();
+		Date expirationDate = new Date(today.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("API do Cyberspace")
+				.setSubject(user.getId().toString())
+				.setIssuedAt(today)
+				.setExpiration(expirationDate)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+}
--- src/main/java/br/dev/rafael/cyberspace/controller/AuthenticationController.java
+++ src/main/java/br/dev/rafael/cyberspace/controller/AuthenticationController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dev.rafael.cyberspace.config.security.TokenService;
+import br.dev.rafael.cyberspace.controller.dto.TokenDto;
 import br.dev.rafael.cyberspace.controller.form.LoginForm;
 
 @RestController
@@ -20,12 +24,21 @@ public class AuthenticationController {
 	@Autowired
 	private AuthenticationManager authManager;
 	
+	@Autowired
+	private TokenService tokenService;
+	
 	@PostMapping
-	public ResponseEntity<?> login(@RequestBody @Valid LoginForm form){
+	public ResponseEntity<TokenDto> login(@RequestBody @Valid LoginForm form){
 		UsernamePasswordAuthenticationToken loginData = form.converter();
-		authManager.authenticate(loginData);
-		return ResponseEntity.ok().build();
+		try {
+			Authentication authentication = authManager.authenticate(loginData);
+			authManager.authenticate(loginData);
+			String token = tokenService.generateToken(authentication);
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+			
+		}
 	}
 	
-
 }
