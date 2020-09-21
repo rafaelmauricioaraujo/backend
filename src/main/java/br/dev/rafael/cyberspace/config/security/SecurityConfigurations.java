2ec8d447985484a307da2ace9584ac25a1c9eb63
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-09-21 20:34:42 -0300

    profile dev and prod configuration

--- src/main/java/br/dev/rafael/cyberspace/config/security/SecurityConfigurations.java
+++ src/main/java/br/dev/rafael/cyberspace/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package br.dev.rafael.cyberspace.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import br.dev.rafael.cyberspace.repository.UserRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
--- src/main/java/br/dev/rafael/cyberspace/controller/AuthenticationController.java
+++ src/main/java/br/dev/rafael/cyberspace/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package br.dev.rafael.cyberspace.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,6 +20,7 @@ import br.dev.rafael.cyberspace.controller.form.LoginForm;
 
 @RestController
 @RequestMapping("/login")
+@Profile("prod")
 public class AuthenticationController {
 	
 	@Autowired
